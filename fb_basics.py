5f73462157e362e55a97cbe2a956ebc3141f6dee
Umur Can Gurelli <umurcg@gmail.com>
2017-03-23 11:34:46 +0300

    fb basics

--- fb_basics.py
+++ fb_basics.py
@@ -1,15 +1,38 @@
 import serial
 import time
+from enum import IntEnum
+
+class farmBot:
+
+	class Axes(IntEnum):
+		X=1
+		Y=2
+		Z=3
 
 
 
-def connectArduino(portName):
-	ser=serial.Serial('/dev/tty'+portName,9600)
-	time.sleep(2)
-	return ser
+	serialConnection=None
+	portName=None
+
+	def __init__(self):
+
+		self.portName='ACM0'
+		self.serialConnection=self.connect_arduino()
 
 
+	def connect_arduino(self):
+		ser=serial.Serial('/dev/tty'+self.portName,9600)
+		time.sleep(2)
+		return ser
 
-#comment
-connectArduino('ACM0').write('5')
+
+	def driveAxis(self, axis, degree):
+		self.serialConnection.write(axis+' '+degree)
+
+	def conevrtDistanceToDegree(self,axis,length):
+		#TODO calculate lengths for each axes from motor rotation
+		return None
+
+	#comment
+	#connectArduino('ACM0').write('5')
 
